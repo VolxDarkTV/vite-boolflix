25654f581fdf7c481919c9e2fbb3a35af663e741
VolxDarkTV <vinvenzo.foti@outlook.it>
2022-12-16 13:05:35 +0100

    add imgUrl to compose the img links

--- src/store.js
+++ src/store.js
@@ -2,6 +2,8 @@ import {reactive} from 'vue'
 export const store = reactive ({
     cardList:[],
     apiURL: 'https://api.themoviedb.org/3/search/movie?api_key=bc9d00f7dce5f741a8f06adada3b3852',
-    imgURL: 'https://image.tmdb.org/t/p/w500',
+    imgURL: 'https://image.tmdb.org/t/p/w300',
     titolo: 'BoolFlix',
+    searchFilm: '',
+    apiQuery: 'query',
 });
