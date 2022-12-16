d90073d8fbaa707c3707a29ee96b5ea0e7681c85
VolxDarkTV <vinvenzo.foti@outlook.it>
2022-12-16 15:14:30 +0100

    fixed width imageUrl

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import {reactive} from 'vue'
 export const store = reactive ({
     cardList:[],
     apiURL: 'https://api.themoviedb.org/3/search/movie?api_key=bc9d00f7dce5f741a8f06adada3b3852',
-    imgURL: 'https://image.tmdb.org/t/p/w300',
+    imgURL: 'https://image.tmdb.org/t/p/w500',
     titolo: 'BoolFlix',
     searchFilm: '',
     apiQuery: 'query',
