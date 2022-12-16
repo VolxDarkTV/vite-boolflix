3d14bde3b3f9039b0f1141e9f57fc1ec23b69b93
VolxDarkTV <vinvenzo.foti@outlook.it>
2022-12-16 17:27:53 +0100

    add series url

--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@ import {reactive} from 'vue'
 export const store = reactive ({
     cardList:[],
     apiURL: 'https://api.themoviedb.org/3/search/movie?api_key=bc9d00f7dce5f741a8f06adada3b3852',
+    apiSeriesURL: 'https://api.themoviedb.org/3/search/tv?api_key=bc9d00f7dce5f741a8f06adada3b3852',
     imgURL: 'https://image.tmdb.org/t/p/w500',
     titolo: 'BoolFlix',
     searchFilm: '',
