698150240c7f37935415496d996494073ef2d5c7
VolxDarkTV <vinvenzo.foti@outlook.it>
2022-12-19 10:15:17 +0100

    modify api link with multi and remove one api link

--- src/store.js
+++ src/store.js
@@ -1,8 +1,9 @@
 import {reactive} from 'vue'
 export const store = reactive ({
     cardList:[],
-    apiURL: 'https://api.themoviedb.org/3/search/movie?api_key=bc9d00f7dce5f741a8f06adada3b3852',
-    apiSeriesURL: 'https://api.themoviedb.org/3/search/tv?api_key=bc9d00f7dce5f741a8f06adada3b3852',
+    // Ho dato al link /multi al posto di /movie o /tv
+    apiURL: 'https://api.themoviedb.org/3/search/multi?api_key=bc9d00f7dce5f741a8f06adada3b3852',
+    // apiSeriesURL: 'https://api.themoviedb.org/3/search/tv?api_key=bc9d00f7dce5f741a8f06adada3b3852',
     imgURL: 'https://image.tmdb.org/t/p/w500',
     titolo: 'BoolFlix',
     searchFilm: '',
